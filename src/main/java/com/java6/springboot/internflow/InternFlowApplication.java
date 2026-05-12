9a51a2ac51588454a0612e42535b9d2fb45a5287
ThienlocTran <tranthienloc21102005@gmail.com>
2026-05-12 19:58:48 +0700

    set up project

---
+++ src/main/java/com/java6/springboot/internflow/InternFlowApplication.java
@@ -0,0 +1,13 @@
+package com.java6.springboot.internflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternFlowApplication.class, args);
+    }
+
+}
